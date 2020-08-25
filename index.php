32efea734609175a3ae8b0469c920f225f721eb5
Sohel Mia <sohelcse1999@gmail.com>
2020-08-25 13:53:34 +0600

    PHP Basic syntax and Datatype variable

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+//variable declaration
+// $ variable name = value ;
+// case
+// sanke case,camelCase StudlyCase
+
+//snake case
+
+//$product_price;
+//$productPrice
+
+//$ProductPrice;
+
+//string
+$greatiing = 'Good afternoon';
+
+
+//float
+$product_price = 10.40;
+
+$qauntity = "100";
+
+var_dump($qauntity);
+//string
+$product_name = 'Mackbook';
+
+
+var_dump($product_name[0]);
+
+
+
+
+//echo $product_price;
+
+//integer
+$quantity = 10;
+
+//boolean ;
+
+$isStock = false;
+
+var_dump($isStock);
+
+echo "<br/>";
+
+//100 10 100
+//echo $greatiing;
+
+
+var_dump($product_price);
+echo "<br/>";
+var_dump($product_name);
+echo "<br/>";
+
+$names = [
+    'jon doe',
+    'shohag',
+    'rakib',
+    10
+];
+
+var_dump($names[0]);
+
+var_dump($names);
+
+
