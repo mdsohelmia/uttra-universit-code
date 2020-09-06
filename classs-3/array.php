c4cc190231d5fdd21ce0b886775b5ba9762b567c
Sohel Mia <sohelcse1999@gmail.com>
2020-09-06 23:40:21 +0600

    class -3

---
+++ classs-3/array.php
@@ -0,0 +1,32 @@
+<?php
+
+//$name = 'name1';
+//$name1 = 'name2';
+//$name2 = 'name3';
+//$name3 = 'name4';
+//$name4 = 'name5';
+//
+//echo $name;
+//numeric index array;
+
+$names = ['name1', 'shuvo', 'jon deo', 'hi'];
+
+$roles = ['admin', 'super_admin', 'editor', 'author'];
+
+//$names[2] = 'Haflu';
+//$names[3] = 'Helo';
+//echo $names[0];
+//echo $names[2];
+//echo $names[3];
+
+//for ($i = 0; $i < count($names); $i++) {
+//    echo $names[$i]."<br/>";
+//}
+$number = [1, 3, 5, 6];
+
+foreach ($names as $name) {
+    echo $name;
+}
+for ($i = 0; $i < 10; $i++) {
+    echo $i;
+}
---
+++ classs-3/controller.php
@@ -0,0 +1,114 @@
+<?php
+//if
+//if else
+//switch case
+
+//for do while while foreach
+
+//$stock = 10;
+//$role = 'admin';
+////var_dump($stock >= 10);
+//
+////if ($stock >= 10) {
+////    echo "Send stock alert 10";
+////    if (mail('soehl@gmsfsail.com', 'stock alert', 'Please stock in product')) {
+////        echo "success";
+////    } else {
+////        echo "fail";
+////    }
+////} elseif ($stock <= 0) {
+////    echo "Send stock alert 0";
+////} elseif ($stock >= 15) {
+////    echo "Send stock alert 15";
+////} else {
+////    echo "fail";
+////}
+
+//$n = 10;
+//
+//if ($n >= 18) {
+//    echo "You are adult";
+//} else {
+//    echo "You are child";
+//}
+//
+//$stock = 10;
+//$role = 'admin';
+//var_dump($stock >= 10);
+
+//if ($stock >= 10) {
+//    echo "Send stock alert 10";
+//    if (mail('soehl@gmsfsail.com', 'stock alert', 'Please stock in product')) {
+//        echo "success";
+//    } else {
+//        echo "fail";
+//    }
+//} elseif ($stock <= 0) {
+//    echo "Send stock alert 0";
+//} elseif ($stock >= 15) {
+//    echo "Send stock alert 15";
+//} else {
+//    echo "fail";
+//}
+
+//$stock = 10;
+//
+//switch ($stock) {
+//    case $stock > 10:
+//        echo "send stock alert";
+//        break;
+//    case $stock > 15:
+//        echo "send stock 15";
+//        break;
+//    default:
+//        echo "fail";
+//}
+
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+//echo "Jon doe";
+
+for ($i = 0; $i <= 4; $i++) {
+    echo "jon Doe <br/>";
+}
+
+// 1 x 1 = 1;
+//1 x 2 = 2
+//1 x 3 = 3
+//1 x 4 = 4
+//1 x 5 = 5
+//1 x 6 = 6
+//1 x 7 = 7
+//1 x 8 = 8
+//1 x 9 = 9
+//1 x 10 = 10
+
+//$number = 5;
+//
+//for ($i = 1; $i <= 10; $i++) {
+//    $result = $i * $number;
+//    echo "{$number} x {$i} = {$result} <br/>";
+//}
+
+//$i = 0;
+//while ($i <= 100) {
+//    mail('soehl@gmsfsail.com', 'stock alert', 'Please stock in product');
+//    $i++;
+//}
+//
+//for ($i = 1; $i <= 10; $i++) {
+//    mail('soehl@gmsfsail.com', 'stock alert', 'Please stock in product');
+//}
+
+
+
+
